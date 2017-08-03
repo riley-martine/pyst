264a1b941e79dbf1efd811882281f57a26d24f20
riley <riley.martine@protonmail.com>
2017-08-03 11:52:38 -0400

    Remove ability to run tests w/o nose

---
+++ pystiler/tests/test_screen.py
@@ -0,0 +1,12 @@
+import unittest
+from pystiler.screen import Screen
+
+class TestScreen(unittest.TestCase):
+
+    def test_create_grid(self):
+        rows, cols = 2, 3
+        s = Screen(rows, cols)
+        self.assertEqual(len(s.grid), rows)
+        self.assertEqual(len(s.grid[0]), cols)
+        self.assertEqual(len(s.grid[1]), cols)
+

7941adf5d7766f034a7304d79919ab6d8e460178
riley <maxm215@gmail.com>
2017-08-03 16:30:10 -0400

    Update tests to use parse_args instead of main

--- pystiler/tests/test_api.py
+++ pystiler/tests/test_api.py
@@ -5,19 +5,19 @@ import pystiler
 class TestParser(unittest.TestCase):
   
     def test_move_parser(self):
-        args = pystiler.api.main(['move', 'left'])
+        args = pystiler.api.parse_args(['move', 'left'])
         self.assertEqual(args.cmd, 'move')
         self.assertEqual(args.location, 'left')
         
     def test_all_directions(self):
         for direction in ['right', 'left', 'bottom', 'top', 'top_left', 'top_right', 'bottom_left', 'bottom_right', 'maximize']:
             with self.subTest(direction=direction):
-                args = pystiler.api.main(['move', direction])
+                args = pystiler.api.parse_args(['move', direction])
                 self.assertEqual(args.cmd, 'move')
                 self.assertEqual(args.location, direction)
 
     def test_explicit_parser(self):
-        args = pystiler.api.main(['explicit', '1', '2', '3', '4', '5', '6'])
+        args = pystiler.api.parse_args(['explicit', '1', '2', '3', '4', '5', '6'])
         self.assertEqual(args.cmd, 'explicit')
         self.assertEqual(args.screen_columns, 1)
         self.assertEqual(args.screen_rows, 2)

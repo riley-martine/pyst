fdf9926dcee635631f44fc7fc4a4e5182da8c969
riley <riley.martine@protonmail.com>
2017-08-03 10:30:52 -0400

    Restructure project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pystiler',
+      version='0.1.1',
+      description='Python simple tiler for non-tiling window managers',
+      url='http://github.com/riley-martine/pystiler',
+      author='Riley Martine',
+      author_email='riley.martine.0@gmail.com',
+      license='MIT',
+      packages=['pystiler'],
+      zip_safe=False)

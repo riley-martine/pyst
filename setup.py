ad489ac0679e7c0b3ac5edd859cf2283620b8a1f
riley <maxm215@gmail.com>
2017-08-03 23:10:59 -0400

    RST not rendering

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='pystiler',
-      version='0.2.3',
+      version='0.2.3.2',
       description='Python simple tiler for non-tiling window managers',
       long_description=readme(),
       classifiers=[

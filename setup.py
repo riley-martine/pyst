13cad5637658ed91ec70440e23e656a0dd8d0fe0
riley <riley.martine@protonmail.com>
2017-08-05 19:30:29 -0400

    Fix pylint warnings

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
+"""Setup for pyst, a simple tiler for non-tiling WMs"""
 from setuptools import setup
 
 def readme():
-    with open('README.rst') as f:
-        return f.read()
+    """Read the readme.rst and returns it (for long_description)."""
+    with open('README.rst') as readme_file:
+        return readme_file.read()
 
-setup(name='pystiler',
-      version='0.2.3.2',
+setup(name='pyst',
+      version='0.2.3.3',
       description='Python simple tiler for non-tiling window managers',
       long_description=readme(),
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: X11 Applications',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: Unix',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Desktop Environment :: Window Managers',
+          'Development Status :: 3 - Alpha',
+          'Environment :: X11 Applications',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: Unix',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Desktop Environment :: Window Managers',
       ],
       keywords='window-manager tiling wm command-line cli',
       url='http://github.com/riley-martine/pystiler',
@@ -26,7 +28,7 @@ setup(name='pystiler',
       packages=['pystiler'],
       test_suite='nose.collector',
       tests_require=['nose'],
-      entry_points = {
+      entry_points={
           'console_scripts': ['pyst=pystiler.api:main'],
       },
       include_package_data=True,

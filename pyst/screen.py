8ba1336adc54062244cacbec0e1ad5a2f6b9e800
riley <maxm215@gmail.com>
2017-08-05 21:04:07 -0400

    Style fixes

--- pyst/screen.py
+++ pyst/screen.py
@@ -9,32 +9,30 @@ DEBUG = False
 
 class Screen(object):
     """Represents the screen."""
-    def __init__(self, rows, cols, padding_bottom=20):
+    def __init__(self, grid_rows, grid_cols, padding_bottom=20):
         """Define the screen grid in pixels based on screen size."""
-        self.rows = rows
-        self.cols = cols
         self.padding_bottom = padding_bottom
 
         # All workspaces and their data
         desk_output = subprocess.getoutput("wmctrl -d").split("\n")
         # All workspace numbers
         # desk_list = [line.split()[0] for line in desk_output]
+        # self.desktop = current[0]
 
         # Data about current desktop. Not sure how it works with multiple.
         current = [line for line in desk_output if line.split()[1] == "*"][0].split()
 
-        self.desktop = current[0]
-        self.width, self.height = map(int, current[8].split('x'))
-        self.orig_x, self.orig_y = current[7].split(',')
+        width, height = [int(x) for x in current[8].split('x')]
+        # self.orig_x, self.orig_y = current[7].split(',')
 
-        row_division = self.height // self.rows
-        col_division = self.width // self.cols
+        row_division = height // grid_rows
+        col_division = width // grid_cols
 
 
         self.grid = []
-        for rownum in range(rows):
+        for rownum in range(grid_rows):
             row = []
-            for colnum in range(cols):
+            for colnum in range(grid_cols):
                 col = []
                 col.append((col_division*colnum, col_division*(colnum+1)))
                 col.append((row_division*rownum, row_division*(rownum+1)))
@@ -60,12 +58,10 @@ class Screen(object):
         col_start, col_end = cols
 
         x_start_coord = self.grid[0][col_start-1][0][0]
-        x_final_coord =  self.grid[0][col_end - 1][0][1]
-        x_coords = (x_start_coord, x_final_coord)
-        
+        x_final_coord = self.grid[0][col_end - 1][0][1]
+
         y_start_coord = self.grid[row_start-1][0][1][0]
         y_final_coord = self.grid[row_end - 1][0][1][1] - self.padding_bottom
-        y_coords = (y_start_coord, y_final_coord)
 
 
         x = x_start_coord
@@ -80,38 +76,39 @@ class Screen(object):
            Precondition: two two-tuples of (start,end) for rows and columns.
         """
         x, y, w, h = self.get_coords(cols, rows)
-        self.move_coords(x, y, w, h)
+        move_coords(x, y, w, h)
 
 
-    def move_coords(self, x, y, w, h):
-        """Move active window to specified coords."""
+def move_coords(x, y, w, h):
+    """Move active window to specified coords."""
 
-        command = ','.join(map(str, [" wmctrl -r :ACTIVE: -e 0", x, y, w, h]))
+    params = ','.join([str(pix) for pix in [x, y, w, h]])
+    command = " wmctrl -r :ACTIVE: -e 0," + params
 
-        if DEBUG:
-            print(command)
+    if DEBUG:
+        print(command)
 
-        os.system(command)
+    os.system(command)
 
 
 
 
 
 if __name__ == "__main__":
-    screen_rows = int(sys.argv[1])
-    screen_cols = int(sys.argv[2])
-    first_col = int(sys.argv[3])
-    last_col = int(sys.argv[4])
-    first_row = int(sys.argv[5])
-    last_row = int(sys.argv[6])
+    GRID_ROWS = int(sys.argv[1])
+    GRID_COLS = int(sys.argv[2])
+    FIRST_COL = int(sys.argv[3])
+    FINAL_COL = int(sys.argv[4])
+    FIRST_ROW = int(sys.argv[5])
+    FINAL_ROW = int(sys.argv[6])
 
-    cols_filled = (first_col, last_col)
-    rows_filled = (first_row, last_row)
+    COLS_FILLED = (FIRST_COL, FINAL_COL)
+    ROWS_FILLED = (FIRST_ROW, FINAL_ROW)
 
-    s = Screen(screen_rows, screen_cols)
+    SCREEN = Screen(GRID_ROWS, GRID_COLS)
 
     if DEBUG:
-        print(c)
+        print(SCREEN.get_coords(ROWS_FILLED, COLS_FILLED))
 
-    s.move_active(rows_filled, cols_filled)
+    SCREEN.move_active(ROWS_FILLED, COLS_FILLED)
 
